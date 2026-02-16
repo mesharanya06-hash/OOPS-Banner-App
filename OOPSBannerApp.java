ea7ce5134a4b818daea9eb99d168391f686bccea
sharanya <mesharanya06@gmail.com>
2026-02-16 21:47:44 +0530

    Added UC2 OOPS banner ASCII feature

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -12,13 +12,12 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" **   **   **    **  ");
-        System.out.println("*     *  *     *  *    *   *      ");
-        System.out.println("*     *  *     *  *    *   *      ");
-        System.out.println("*     *  **   **     ***  ");
-        System.out.println("*     *  *        *              *");
-        System.out.println("*     *  *        *              *");
-        System.out.println(" **   *        *        **  ");
-
+    System.out.println(" *****   *****   ******   ***** ");
+    System.out.println("*     * *     *  *     *  *     *");
+    System.out.println("*     * *     *  *     *  *");
+    System.out.println("*     * *     *  ******    ***** ");
+    System.out.println("*     * *     *  *              *");
+    System.out.println("*     * *     *  *        *     *");
+    System.out.println(" *****   *****   *         ***** ");
     }
 }
